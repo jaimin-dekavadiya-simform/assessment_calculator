03764febe842def6917adc81961de72377aa0aba
jaimin-dekavadiya-simform <jaimin.dekavadiya@simformsolutions.com>
2026-02-23 14:09:05 +0530

    feat:Implemented basic structure of tokenizer

---
+++ src/calculator/tokenizer.js
@@ -0,0 +1,28 @@
+export default class Tokenizer {
+  constructor(operators, functions, constants) {
+    this.operators = operators;
+    this.functions = functions;
+    this.constants = constants;
+  }
+  tokenize(str) {
+    const tokens = [];
+    let index = 0;
+    while(index < str.length){
+
+    }
+    return tokens;
+  }
+  #readNumber(str,startIndex){
+
+  }
+  #readOperator(str,startIndex){
+
+  }
+  #readParenthesis(str,startIndex){
+
+  }
+  #readFunction(str,startIndex){
+
+  }
+  #readConstant(str,startIndex)
+}
